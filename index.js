31f0afab8f24345f6560b545d3a3deb3fef56c1c
Franklin Yin <yin4thewin@gmail.com>
2020-12-09 18:27:43 -0500

    Checks for new values each scrape

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 require('dotenv').config()
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
-let checkAndPush=(rows)=>{
+let checkAndPush=(rows, old)=>{
   let newChanges=[]
+  //Look at info from previous scrape and remove from current
+  // let oldNames=old.map(el=>{
+  //   return el.name
+  // })
+  // oldNames.forEach(e=>{
+  //   console.log(e)
+  // })
+  old.forEach(oldName=>{
+    rows.forEach((el,i)=>{
+      if(oldName.name===el.name){
+        rows.splice(i, 1)
+      }
+    })
+  })
   console.log("Loaded "+doc.title);
   rows.forEach(el=>{
     let yokaiObj={
@@ -15,7 +29,8 @@ let checkAndPush=(rows)=>{
       soult: el.soult,
       skill: el.skill,
       g: el.g,
-      series: el.series
+      series: el.series,
+      image: el.img
     }
     newChanges.push(yokaiObj)
   })
@@ -27,7 +42,8 @@ let checkAndPush=(rows)=>{
   await doc.useServiceAccountAuth(require('./credential.json'))
   await doc.loadInfo();
   const sheet = doc.sheetsByIndex[0]
+  const old = await doc.sheetsByIndex[1].getRows();
   const rows = await sheet.getRows();
-  checkAndPush(rows)
+  checkAndPush(rows, old)
   setInterval(function(){checkAndPush(rows)}, 50000)
 })()
